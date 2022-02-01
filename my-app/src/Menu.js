7df2ff5a06b004c8bf1ecb818415629cc4a6a729
Mabchir <38863638+Mabchir@users.noreply.github.com>
2022-02-01 13:29:55 -0500

    Finish Menu (Basic functions)
    - [x]  take out bullet points
    - [x]  align menu elements
    - [x]  add empty link to the menu elements
    - [x]  bold the home element
    - [x]  add some styling :
        - [x]  font
        - [x]  hover (underline)
    - [x] position menu relative to SVG
    - [x] Make menu responsive (iPad air and iPhone pro screen sizes)

--- my-app/src/Menu.js
+++ my-app/src/Menu.js
@@ -1,34 +1,50 @@
 import React from "react";
 import "./Menu.css";
-
+import MenuBar from "./images/menu-svgrepo-com.svg";
 export default function Menu() {
+  function showMenu() {
+    var x = document.getElementById("menus");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
   return (
     <div className="Menu">
-      Download
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#2F2E41"
-          fill-opacity="1"
+          fillOpacity="1"
           d="M0,96L0,96L480,96L480,160L960,160L960,224L1440,224L1440,0L960,0L960,0L480,0L480,0L0,0L0,0Z"
         ></path>
       </svg>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <ul>
-        <li>Education</li>
-        <li>Portfolio</li>
-        <li>Work</li>
-      </ul>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#2F2E41"
-          fill-opacity="1"
-          d="M0,96L0,96L480,96L480,160L960,160L960,224L1440,224L1440,320L960,320L960,320L480,320L480,320L0,320L0,320Z"
-        ></path>
-      </svg>
+      <img src={MenuBar} onClick={showMenu} />
+
+      <div id="menus">
+        <ul id="part1">
+          <a href="#">
+            <li className="current">Home</li>
+          </a>
+          <a href="#">
+            <li>About</li>
+          </a>
+          <a href="#">
+            <li>Contact</li>
+          </a>
+        </ul>
+        <ul id="part2">
+          <a href="#">
+            <li>Education</li>
+          </a>
+          <a href="#">
+            <li>Portfolio</li>
+          </a>
+          <a href="#">
+            <li>Work</li>
+          </a>
+        </ul>
+      </div>
     </div>
   );
 }
