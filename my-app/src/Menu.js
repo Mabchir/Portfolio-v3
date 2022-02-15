dbcaf72db0f8aa0a8112cdcca64b5e3bdbfe60d3
Mabchir <38863638+Mabchir@users.noreply.github.com>
2022-02-14 19:38:33 -0500

    fixing the responsiveness of home page on phone screens

--- my-app/src/Menu.js
+++ my-app/src/Menu.js
@@ -2,18 +2,7 @@ import React from "react";
 import "./Menu.css";
 import { Outlet, NavLink } from "react-router-dom";
 
-import MenuBar from "./images/menu-svgrepo-com.svg";
-
 export default function Menu(currentPage) {
-  function showMenu() {
-    var x = document.getElementById("menus");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
   return (
     <div className="Menu">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -23,7 +12,6 @@ export default function Menu(currentPage) {
           d="M0,96L0,96L480,96L480,160L960,160L960,224L1440,224L1440,0L960,0L960,0L480,0L480,0L0,0L0,0Z"
         ></path>
       </svg>
-      <img src={MenuBar} onClick={showMenu} alt="menubar" />
       <div id="menus">
         <nav id="part1">
           <NavLink
